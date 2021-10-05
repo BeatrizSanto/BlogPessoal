42e854b7b2160a1e22e500a68a3a3fe69a2da947
Beatriz Santos <beatriz.santos19@yahoo.com.br>
2021-10-05 17:58:04 -0300

    Componente inicial sem consumo

--- src/app/service/auth.service.ts
+++ src/app/service/auth.service.ts
@@ -1,6 +1,7 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
+import { environment } from './../../environments/environment.prod';
 import { User } from '../model/User';
 import { UserLogin } from '../model/UserLogin';
 
@@ -19,5 +20,13 @@ return this.http.post<UserLogin>('http://localhost:8080/usuarios/logar', userLog
   
   cadastrar(user: User):Observable<User>{
     return this.http.post<User>('http://localhost:8080/usuarios/cadastrar',user)
+  }
+  logado(){
+    let ok: Boolean = false
+
+    if(environment.token != ''){
+    ok=true
+    }
+    return ok
   }
 }
